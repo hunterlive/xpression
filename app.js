0ab7b3250cdf6a55d0935d099bcfc9a29187687b
Justinian Huntz <justinhunter1@live.com>
2015-07-29 19:20:46 -0400

    some things added

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var app = express();
 
+
+app.set ('view engine', 'ejs');
+
+//app.set ( 'views', __dirname + '/views' );
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('default', { title: 'Home'});
 });
 
+app.get('/sea', function (req, res) {
+  res.send('Hello SEA');
+});
+
+app.get('/space', function (req, res) {
+  res.send('HelloSPACE');
+});
+
+app.get('/sky', function (req, res) {
+  res.send('Hello SKY');
+});
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
